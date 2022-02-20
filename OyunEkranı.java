488ab0431d5c5767ba759447c6ba2847559ba2f9
doga.nur <dcamdereli@gmail.com>
2022-02-20 16:17:37 +0300

    OyunEkranı.java

---
+++ OyunEkranı.java
@@ -0,0 +1,38 @@
+package UzayOyunu;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class OyunEkranı extends JFrame{
+
+    
+    public OyunEkranı(String title) throws HeadlessException {
+        super(title);
+    }
+
+    public static void main(String[] args) {
+        OyunEkranı ekran = new OyunEkranı("UZAY OYUNU");
+
+        ekran.setResizable(false);
+        ekran.setFocusable(false);
+        ekran.setSize(800,600);
+
+        ekran.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Oyun oyun = new Oyun();
+        oyun.requestFocus();
+
+        oyun.addKeyListener(oyun);
+
+        oyun.setFocusable(true);
+        oyun.setFocusTraversalKeysEnabled(false);
+
+        ekran.add(oyun);
+
+        ekran.setVisible(true);
+
+    }
+    
+    
+}
